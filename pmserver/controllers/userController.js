0488ac4bbcc973017d2e8ab6b40f4a75b5bd47a3
tnlong1997 <tnlong1997@gmail.com>
2018-04-10 00:10:02 -0400

    Add user login in userController

--- pmserver/controllers/userController.js
+++ pmserver/controllers/userController.js
@@ -60,3 +60,61 @@ exports.user_sign_up = function(req, res, next) {
 
   });
 }
+
+// User login
+exports.user_log_in = function(req, res, next) {
+
+  if (!req.body.email) {
+    return res.send({
+      success: false,
+      code: 400,
+      err: "No email in input"
+    });
+  }
+
+  if (!req.body.password) {
+    return res.send({
+      success: false,
+      code: 400,
+      err: "No password in input"
+    })
+  }
+
+  User.findOne({username: req.body.email})
+    .exec(function(err, user) {
+      if (err) {
+        return res.send({
+          success: false,
+          code: 400,
+          err: err
+        });
+      }
+
+      if (!user) {
+        return res.send({
+          success: false,
+          code: 400,
+          err: "Authenticate failed. User not found"
+        });
+      }
+
+      user.comparePassword(req.body.password, function(err, isMatch) {
+        if (isMatch && !err) {
+          // Create token if the password matched and no error was thrown
+          var token = jwt.sign({username: user.email, _id: user._id}, secret, {
+            expiresIn: 1000000000000000 // in seconds
+          });
+          res.json({
+            success: true,
+            code: 200,
+            token: token,
+          });
+        } else {
+          res.send({
+            success: false,
+            message: 'Authentication failed. Passwords did not match.'
+          });
+        }
+      })
+    });
+}
