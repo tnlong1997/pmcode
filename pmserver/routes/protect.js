1de5180e6955a322b4ff5de698e83bf38205466c
tnlong1997 <tnlong1997@gmail.com>
2018-04-18 00:11:37 -0400

    Fix protect.js lint

--- pmserver/routes/protect.js
+++ pmserver/routes/protect.js
@@ -6,9 +6,9 @@ var tokenAuthentication = require('../middlewares/tokenAuthentication');
 // Add tokenAuthentication as middleware
 router.use(tokenAuthentication);
 
-router.get('/', function(req, res, next) {
-  // For testing
-  return res.send('Protected primor routes');
+router.get('/', function(req, res) {
+	// For testing
+	return res.send('Protected primor routes');
 });
 
 module.exports = router;
