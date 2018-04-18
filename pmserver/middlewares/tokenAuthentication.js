f79e161e7af3abd54e2dfcd2f9327aede2373a91
tnlong1997 <tnlong1997@gmail.com>
2018-04-18 01:33:29 -0400

    fix tokenAuthentiacation  lint

--- pmserver/middlewares/tokenAuthentication.js
+++ pmserver/middlewares/tokenAuthentication.js
@@ -3,36 +3,36 @@ var jwt = require('jsonwebtoken');
 var secret = require('../config/secret');
 
 module.exports = function(req, res, next) {
-  // check header or url parameters or post parameters for token
-  var token = req.body.token || req.query.token || req.headers['jwt'];
+	// check header or url parameters or post parameters for token
+	var token = req.body.token || req.query.token || req.headers['jwt'];
 
-  // decode token
-  if (token) {
+	// decode token
+	if (token) {
 
-    // verifies secret and checks exp
-    jwt.verify(token, secret, function(err, decoded) {
-      if (err) {
-        return res.send({
-          success: false,
-          code: 400,
-          err: 'Failed to authenticate token.'
-        });
-      } else {
-        // if everything is good, save to request for use in other routes
-        req.decoded = decoded;
-        next();
-      }
-    });
+		// verifies secret and checks exp
+		jwt.verify(token, secret, function(err, decoded) {
+			if (err) {
+				return res.send({
+					success: false,
+					code: 400,
+					err: 'Failed to authenticate token.'
+				});
+			} else {
+				// if everything is good, save to request for use in other routes
+				req.decoded = decoded;
+				next();
+			}
+		});
 
-  } else {
+	} else {
 
-    // if there is no token, return an error
-    return res.send({
-        success: false,
-        code: 400,
-        err: 'No token provided.'
-    });
+		// if there is no token, return an error
+		return res.send({
+			success: false,
+			code: 400,
+			err: 'No token provided.'
+		});
 
-  }
+	}
 
-}
+};
