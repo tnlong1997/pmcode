ae6e5dee5e2fa0635c21b6cd0d242b28436c8c71
tnlong1997 <tnlong1997@gmail.com>
2018-04-24 19:22:01 -0400

    Add users test data

---
+++ pmserver/test/unittest/public/variables/users_test_data.js
@@ -0,0 +1,49 @@
+exports.testUser = {
+	email: 'testUser@gmail.com',
+	password: 'testUser',
+	profile: {
+		firstName: 'test',
+		lastName: 'user',
+	}
+};
+
+exports.wrongEmailFormatUser = {
+	email: 'testUser',
+	password: 'testUser',
+	profile: {
+		firstName: 'test',
+		lastName: 'user',
+	}
+};
+
+exports.noEmailUser = {
+	password: 'testUser',
+	profile: {
+		firstName: 'test',
+		lastName: 'user',
+	}
+};
+
+exports.noPasswordUser = {
+	email: 'testUser',
+	profile: {
+		firstName: 'test',
+		lastName: 'user',
+	}
+};
+
+exports.noLastNameUser = {
+	email: 'testUser',
+	password: 'testUser',
+	profile: {
+		firstName: 'test',
+	}
+};
+
+exports.noFirstNameUser = {
+	email: 'testUser',
+	password: 'testUser',
+	profile: {
+		lastName: 'user',
+	}
+};
