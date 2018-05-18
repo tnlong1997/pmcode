4d0c11a51797c16d2e15afccadaf5f977211f936
tnlong1997 <tnlong1997@gmail.com>
2018-05-18 10:40:47 +0700

    Change middlewares to middleware

--- pmserver/controllers/userController.js
+++ pmserver/controllers/userController.js
@@ -2,7 +2,7 @@ var User = require('../models/userModel');
 var bcrypt = require('bcrypt-nodejs');
 var secret = require('../config/secret');
 var jwt = require('jsonwebtoken');
-var sign_up_email = require('../middlewares/emails/sign-up-email');
+var sign_up_email = require('../middleware/emails/sign-up-email');
 
 // Display all user
 exports.user_list = function(req, res) {
