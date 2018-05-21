eab9a42bcd9dd9c8f9cbec3a027ce5bb63d89aa4
tnlong1997 <tnlong1997@gmail.com>
2018-05-21 14:06:04 +0700

    Fix reset url

--- pmserver/middleware/emails/forgot-password-email.js
+++ pmserver/middleware/emails/forgot-password-email.js
@@ -11,7 +11,7 @@ module.exports = function(receiverEmail, token, next) {
 		subject: 'Primor Password Reset',
 		text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
           'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-          'localhost:3000/' + 'public/reset/' + token + '\n\n' +
+          'localhost:3000/' + 'public/users/reset/' + token + '\n\n' +
           'If you did not request this, please ignore this email and your password will remain unchanged.\n',
 	};
 
