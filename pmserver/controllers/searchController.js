17934d8bf54dec129168a000bf70b3532e38ea28
Hoang Nguyen <hoangnguyenthai97@gmail.com>
2018-05-25 13:50:40 +0700

    Add search helpers

--- pmserver/controllers/searchController.js
+++ pmserver/controllers/searchController.js
@@ -1,9 +1,8 @@
-var Order = require('../models/orderModel');
 var Item = require('../models/itemModel');
+var search_helpers = require('../middleware/helpers/search_helpers');
 
 exports.search = function(req, res) {
-	if (req.body.search_string 
-	&& req.body.search_item_price_gte
+	if (req.body.search_item_price_gte
 	&& req.body.search_item_price_lte
 	&& req.body.search_traveler_fee_gte
 	&& req.body.search_traveler_fee_lte
@@ -11,51 +10,27 @@ exports.search = function(req, res) {
 	&& req.body.search_date_lte
 	&& req.body.search_receiver_country) {
 
-		Item.find({
-			$text: {
-				$search: req.body.search_string
-			},
-			item_price: { 
-				$gte: req.body.search_item_price_gte, 
-				$lte: req.body.search_item_price_lte
-			}
-		}, function(err, matched_items) {
-			if (err) {
-				res.send({
-					success: false,
-					code: 600,
-					status: "Database Error Item",
-				});
-			}
+		if (req.body.search_string) {
+			Item.find({
+				$text: {
+					$search: req.body.search_string
+				},
+				item_price: { 
+					$gte: req.body.search_item_price_gte, 
+					$lte: req.body.search_item_price_lte
+				}
+			}, function(err, matched_items) {
+				if (err) {
+					res.send({
+						success: false,
+						code: 600,
+						status: "Database Error Item",
+					});
+				}
 
-			if (matched_items.length == 0) {
-				console.log(1);
-				Order.find({
-					$text: {
-						$search: req.body.search_string
-					},
-					traveler_fee: { 
-						$gte: req.body.search_traveler_fee_gte, 
-						$lte: req.body.search_traveler_fee_lte
-					},
-					required_date_from: {
-						$not: { $gt: req.body.search_date_lte }
-					},
-					required_date_to: {
-						$not: { $lt: req.body.search_date_gte }
-					},
-					receiver_country: req.body.search_country,
-					$comment: "Suggestion queries"
-				}, function(err_2, matched_orders) {
-					if (err_2) {
-						res.send({
-							success: false,
-							code: 600,
-							status: "Database Error Suggestion"
-						});
-					}
-
-					if (matched_orders.length == 0) {
+				if (matched_items.length == 0) {
+					var suggested_orders = search_helpers.find_suggested_order_with_search_string(req, res);
+					if (suggested_orders.length == 0) {
 						res.send({
 							success: true,
 							code: 200,
@@ -66,55 +41,67 @@ exports.search = function(req, res) {
 							success: true,
 							code: 200,
 							status: "Show suggestion orders",
+							orders: suggested_orders
+						});
+					}
+				} else {
+					var matched_orders = search_helpers.find_matched_order(req, res, matched_items);
+					if (matched_orders.length == 0) {
+						res.send({
+							success: true,
+							code: 200,
+							status: "No matching order"
+						});
+					} else {
+						res.send({
+							success: true,
+							code: 200,
+							status: "Show results",
 							orders: matched_orders
 						});
 					}
-				});
-
-			} else {
-				for (var item in matched_items) {
-					Order.find({
-						item: matched_items[item]._id,
-						traveler_fee: { 
-							$gte: req.body.search_traveler_fee_gte, 
-							$lte: req.body.search_traveler_fee_lte
-						},
-						required_date_from: {
-							$not: { $gt: req.body.search_date_lte }
-						},
-						required_date_to: {
-							$not: { $lt: req.body.search_date_gte }
-						},
-						receiver_country: req.body.search_country,
-						$comment: "Matched item queries"
-					}, function(err_2, matched_orders) {
-						if (err_2) {
-							res.send({
-								success: false,
-								code: 600,
-								status: "Database Error Matched"
-							});
-						}
-						
-						if (matched_orders.length == 0) {
-							res.send({
-								success: true,
-								code: 200,
-								status: "No matching order"
-							});
-						} else {
-							res.send({
-								success: true,
-								code: 200,
-								status: "Show results",
-								orders: matched_orders
-							});
-						}
-						
+				}
+			});
+		} else {
+			Item.find({
+				item_price: { 
+					$gte: req.body.search_item_price_gte, 
+					$lte: req.body.search_item_price_lte
+				}
+			}, function(err, matched_items) {
+				if (err) {
+					res.send({
+						success: false,
+						code: 600,
+						status: "Database Error Item",
 					});
 				}
-			}
-		});
+
+				if (matched_items.length == 0) {
+					res.send({
+						success: true,
+						code: 200,
+						status: "No matching items"
+					});
+				} else {
+					var matched_orders = search_helpers.find_matched_order(req, res, matched_items);
+					if (matched_orders.length == 0) {
+						res.send({
+							success: true,
+							code: 200,
+							status: "No matching order"
+						});
+					} else {
+						res.send({
+							success: true,
+							code: 200,
+							status: "Show results",
+							orders: matched_orders
+						});
+					}
+				}
+			});
+		}
 
 	} else {
 		res.send({
