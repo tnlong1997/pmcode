d37e23a6ccbdf378499e9d712a4fea8d954024a0
Hoang Nguyen <hoangnguyenthai97@gmail.com>
2018-05-25 11:39:03 +0700

    Update search algo

--- pmserver/routes/search.js
+++ pmserver/routes/search.js
@@ -3,16 +3,6 @@ var router = express.Router();
 
 const searchController = require('../controllers/searchController');
 
-router.post('/order-name', searchController.search_order_name);
-
-router.post('/item-name', searchController.search_item_name);
-
-router.post('/item-price', searchController.search_item_price);
-
-router.post('/traveler-fee', searchController.search_traveler_fee);
-
-router.post('/date', searchController.search_date);
-
-router.post('/receiver_country', searchController.search_receiver_country);
+router.post('/', searchController.search);
 
 module.exports = router;
